84f3ed3bc1a1e7ee5102d2c7b29c8e7b252cff7d
arul <syahruladhim101@gmail.com>
2023-01-17 12:13:37 +0700

    BELUMSELESE

--- src/JAWABANO3.java
+++ src/JAWABANO3.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class JAWABANO3 {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class JAWABANNO3 {
+    public static void main (String[]  args) {
+        int jum=4;
+        UASID mymessage = new UASID();
+        Scanner sc = new Scanner(System.in);
+        mymessage.setMessageID(sc.nextLine());
+        mymessage.setMessageName(sc.nextLine());
+        ArrayList<String> context = new ArrayList<>();
+        for (int index=0; index<jum;index++) {
+            context.add(sc.nextLine());
+        }
+        mymessage.setContext(context);
+        System.out.println(" kata yang dicari : ");
+        String cari = sc.nextLine();
+        System.out.println(mymessage.Findwords(context,cari));// output "Index ke 0 1"
+    }
 }
--- src/UASID.java
+++ src/UASID.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class UASID {
+import java.util.ArrayList;
+
+public class UASID {
+    public String getMessageID() {
+        return MessageID;
+    }
+
+    public void setMessageID(String messageID) {
+        MessageID = messageID;
+    }
+
+    public String getMessageName() {
+        return MessageName;
+    }
+
+    public void setMessageName(String messageName) {
+        MessageName = messageName;
+    }
+
+    public ArrayList getContext() {
+        return Context;
+    }
+
+    public void setContext(ArrayList context) {
+        Context = context;
+    }
+
+    String MessageID;
+    String MessageName;
+    ArrayList Context;
 }
